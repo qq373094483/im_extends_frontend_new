fb9a7dcd32d0719faf89f3530f04b5b66edbbc0a
黄泳源 <hxyas@qq.com>
2023-11-14 23:45:57 +0800

    优化dbUtils

--- js/dbUtils.js
+++ js/dbUtils.js
@@ -2,15 +2,15 @@ var DBUtils={
 
 	getStreamId:function(){
 		var  key ="streamId_";
-		return dbStorage.getItem(key);
+		return window.dbStorage.getItem(key);
 	},
 	setStreamId:function(value){
 		var  key ="streamId_";
-		dbStorage.setItem(key,value);
+		window.dbStorage.setItem(key,value);
 	},
 	getIsEncrypt:function(){
 		var  key ="isEncrypt_";
-		var value=dbStorage.getItem(key);
+		var value=window.dbStorage.getItem(key);
 		if(myFn.isNil(value)){
 			value=0;
 		}else{
@@ -20,11 +20,11 @@ var DBUtils={
 	},
 	setIsEncrypt:function(value){
 		var  key ="isEncrypt_";
-		dbStorage.setItem(key,value);
+		window.dbStorage.setItem(key,value);
 	},
 	getLogoutTime:function(){
 		var  key ="logOutTime_";
-		var value=dbStorage.getItem(key);
+		var value=window.dbStorage.getItem(key);
 		if(myFn.isNil(value)){
 			value=0;
 		}else{
@@ -34,62 +34,70 @@ var DBUtils={
 	},
 	setLogoutTime:function(time){
 		var  key ="logOutTime_";
-		dbStorage.setItem(key,time);
+		window.dbStorage.setItem(key,time);
 	},
 	/*获取好友聊天记录*/
 	getMsgRecordList:function(userId){
 		var key="msgRecordList_"+userId;
-		var arrList=dbStorage.getItem(key);
-		if(myFn.isNil(arrList))
-			arrList=new Array();
-		else
-			arrList=JSON.parse(arrList);
+		var arrList=window.dbStorage.getItem(key);
+		if(myFn.isNil(arrList)) {
+			arrList = new Array();
+		}
+		else {
+			arrList = JSON.parse(arrList);
+		}
 		return arrList;
 	},
 	setMsgRecordList:function(userId,arrList){
 		var key="msgRecordList_"+userId;
-		dbStorage.setItem(key,JSON.stringify(arrList));
+		window.dbStorage.setItem(key,JSON.stringify(arrList));
 	},
 	putMsgRecordList:function(userId,msgId){
 		var key="msgRecordList_"+userId;
 		var arrList=DBUtils.getMsgRecordList(userId);
 		arrList.push(msgId);
-		dbStorage.setItem(key,JSON.stringify(arrList));
+		window.dbStorage.setItem(key,JSON.stringify(arrList));
 	},
 	clearMsgRecordList:function(userId){
 		var key="msgRecordList_"+userId;
-		dbStorage.removeItem(key);
+		window.dbStorage.removeItem(key);
 	},
+	//获取缓存中的消息总数
 	getMsgNumCount:function(){
 		var key = "msgNumCount_";
-		var num=dbStorage.getItem(key);
+		var num=window.dbStorage.getItem(key);
 		if(myFn.isNil(num)){
 			num=0;
 		}
 		num=parseInt(num);
 		return num;
 	},
+	//设置缓存中的消息总数
 	setMsgMumCount:function(num){
-		var key="msgNumCount_"
-		dbStorage.setItem(key,num);
+		var key = "msgNumCount_";
+		window.dbStorage.setItem(key,num);
 	},
+	//获取好友缓存中的消息
 	getMsgNum:function(id){
 		var key="msgNum_"+id;
-		var num=dbStorage.getItem(key);
-		if(myFn.isNil(num))
-			num=0;
-		else
-			num=parseInt(num);
+		var num=window.dbStorage.getItem(key);
+		if(myFn.isNil(num)) {
+			num = 0;
+		}
+		else {
+			num = parseInt(num);
+		}
 		return num;
 	},
+	//设置好友缓存中的消息
 	setMsgNum:function(id,num){
 		var key="msgNum_"+id;
-		dbStorage.setItem(key,num);
+		window.dbStorage.setItem(key,num);
 	},
 	/*获取最近的消息列表记录*/
 	getUIMessageList:function(){
 		var key="messageList_";
-		var messageList=dbStorage.getItem(key);
+		var messageList=window.dbStorage.getItem(key);
 		//tigLog("getUIMessageList ===> "+messageList);
 		if(myFn.isNil(messageList)){
 			return null;
@@ -107,7 +115,7 @@ var DBUtils={
 		/*var temp=messageList[msg.id];*/
 		
 		messageList[msg.id]=msg;
-		dbStorage.setItem(key,JSON.stringify(messageList));
+		window.dbStorage.setItem(key,JSON.stringify(messageList));
 	},
 	removeUIMessageList:function(userId){
 		var key="messageList_";
@@ -117,7 +125,7 @@ var DBUtils={
 		}
 		delete messageList[userId];
 
-		dbStorage.setItem(key,JSON.stringify(messageList));
+		window.dbStorage.setItem(key,JSON.stringify(messageList));
 	},
 	getLastMsg:function(userId){
 		var messageList=DBUtils.getUIMessageList();
@@ -130,19 +138,19 @@ var DBUtils={
 		return "msg_"+messageId;
 	},
 	getMessage:function(messageId){
-		var msg=dbStorage.getItem(this.getMessageKey(messageId));
+		var msg=window.dbStorage.getItem(this.getMessageKey(messageId));
 		msg=JSON.parse(msg);
 		return msg;
 	},
 	saveMessage:function(msg){
-		dbStorage.setItem(this.getMessageKey(msg.messageId),JSON.stringify(msg));
+		window.dbStorage.setItem(this.getMessageKey(msg.messageId),JSON.stringify(msg));
 	},
 	deleteMessage:function(messageId){
-		return dbStorage.removeItem(this.getMessageKey(messageId));
+		return window.dbStorage.removeItem(this.getMessageKey(messageId));
 	},
 	getMsgReadList:function(messageId){
 		var key="msgReadList_"+messageId;
-		var readList=dbStorage.getItem(key);
+		var readList=window.dbStorage.getItem(key);
 		if(myFn.isNil(readList)){
 			readList=new Array();
 		}else{
@@ -154,7 +162,7 @@ var DBUtils={
 	putMsgReadList:function(messageId,msg){
 		/*msg={id:"",userId:"",nickname,"",timeSend:100}*/
 		var key="msgReadList_"+messageId;
-		var readList=dbStorage.getItem(key);
+		var readList=window.dbStorage.getItem(key);
 		if(myFn.isNil(readList)){
 			readList=new Array();
 		}else{
@@ -162,60 +170,63 @@ var DBUtils={
 		}
 		readList.push(msg);
 
-		dbStorage.setItem(key,JSON.stringify(readList));
+		window.dbStorage.setItem(key,JSON.stringify(readList));
 	},
 	/*获取消息已读数量*/
 	getMsgReadNum:function (messageId) {
 		//设置已读数量
 		var key="msgReadNum_"+messageId;
-		var value= dbStorage.getItem(key);
-		if(myFn.isNil(value))
-			value=0;
-		else
-			value=parseInt(value);
+		var value= window.dbStorage.getItem(key);
+		if(myFn.isNil(value)) {
+			value = 0;
+		}
+		else {
+			value = parseInt(value);
+		}
 		return value;
 	},
 	//更新消息已读数量
 	setMsgReadNum:function (messageId,num) {
 		//设置已读数量
 		var key="msgReadNum_"+messageId;
-		 dbStorage.setItem(key,num);
+		window.dbStorage.setItem(key,num);
 		
 	},
 	/*获取消息过滤状态*/
 	getMsgFilters:function(id){
-		var status=dbStorage.getItem(id);
-		if(myFn.isNil(status))
-			status="0";
+		var status=window.dbStorage.getItem(id);
+		if(myFn.isNil(status)) {
+			status = "0";
+		}
 		return status;
 	
 	},
 	setMsgFilters:function(id,status){
 		GroupManager.filters[id]=status;
-		dbStorage.setItem(id,status);
+		window.dbStorage.setItem(id,status);
 	},
 
 	getStr:function(key){
-		return dbStorage.getItem(key);
+		return window.dbStorage.getItem(key);
 	},
 	saveStr:function(key,value){
-		return dbStorage.setItem(key,value);
+		return window.dbStorage.setItem(key,value);
 	},
 	getObj:function(key){
-		var value=dbStorage.getItem(key);
-		if(isJSON(str)){
-			value=JSON.parse(msg);
+		var value=window.dbStorage.getItem(key);
+		if(isJSON(value)){
+			value=JSON.parse(value);
 		}
 		return value;
 	},
 	saveObj:function (key,obj) {
-		dbStorage.setItem(key,JSON.stringify(obj));
+		window.dbStorage.setItem(key,JSON.stringify(obj));
 	},
 	deleteObj:function(key){
-	   return dbStorage.removeItem(key);
+	   return window.dbStorage.removeItem(key);
 	},
 	clearAll:function(){
-		dbStorage.clear();
+		window.dbStorage.clear();
 	}
 };
 
@@ -234,5 +245,5 @@ function isJSON(str) {
             return false;
         }
     }
-    console.log('It is not a string!')
+	console.log('It is not a string!');
 }
